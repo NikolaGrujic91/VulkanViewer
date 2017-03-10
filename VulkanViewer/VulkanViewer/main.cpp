63ca7732f86ca096608cf3365a7e432268acb551
Nikola Grujić <grujic.nikola91@gmail.com>
2017-03-10 20:24:55 +0100

    Attribute descriptions
    Attribute description struct describes how to extract a vertex attribute
    from a chunk of vertex data originating from a binding description. We
    have two attributes, position and color, so we need two attribute
    description structs.

--- VulkanViewer/VulkanViewer/main.cpp
+++ VulkanViewer/VulkanViewer/main.cpp
@@ -8,6 +8,7 @@
 #include <vector>
 #include <cstring>
 #include <set>
+#include <array>
 #include <glm/glm.hpp>
 
 const int WIDTH = 800;
@@ -38,6 +39,19 @@ struct Vertex {
 		bindingDescription.stride = sizeof(Vertex);
 		bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
 		return bindingDescription;
+	}
+
+	static std::array<VkVertexInputAttributeDescription, 2> getAttributeDescriptions() {
+		std::array<VkVertexInputAttributeDescription, 2> attributeDesriptions = {};
+		attributeDesriptions[0].binding = 0;
+		attributeDesriptions[0].location = 0;
+		attributeDesriptions[0].format = VK_FORMAT_R32G32B32A32_SFLOAT;
+		attributeDesriptions[0].offset = offsetof(Vertex, pos);
+		attributeDesriptions[1].binding = 0;
+		attributeDesriptions[1].location = 1;
+		attributeDesriptions[1].format = VK_FORMAT_R32G32B32A32_SFLOAT;
+		attributeDesriptions[1].offset = offsetof(Vertex, color);
+		return attributeDesriptions;
 	}
 };
 
