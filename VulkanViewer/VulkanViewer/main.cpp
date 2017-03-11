7d09f0ac911585325824e8f9a2e4d686d16bd63e
Nikola Grujić <grujic.nikola91@gmail.com>
2017-03-11 20:20:29 +0100

    Texture coordinates
    Texture mapping for actual coordinates for each vertex. The coordinates
    determine how the image is actually mapped to the geometry.

--- VulkanViewer/VulkanViewer/main.cpp
+++ VulkanViewer/VulkanViewer/main.cpp
@@ -37,6 +37,7 @@ struct SwapChainSupportDetails {
 struct Vertex {
 	glm::vec2 pos;
 	glm::vec3 color;
+	glm::vec2 texCoord;
 
 	static VkVertexInputBindingDescription getBindingDescription() {
 		VkVertexInputBindingDescription bindingDescription = {};
@@ -46,17 +47,21 @@ struct Vertex {
 		return bindingDescription;
 	}
 
-	static std::array<VkVertexInputAttributeDescription, 2> getAttributeDescriptions() {
-		std::array<VkVertexInputAttributeDescription, 2> attributeDesriptions = {};
-		attributeDesriptions[0].binding = 0;
-		attributeDesriptions[0].location = 0;
-		attributeDesriptions[0].format = VK_FORMAT_R32G32B32A32_SFLOAT;
-		attributeDesriptions[0].offset = offsetof(Vertex, pos);
-		attributeDesriptions[1].binding = 0;
-		attributeDesriptions[1].location = 1;
-		attributeDesriptions[1].format = VK_FORMAT_R32G32B32A32_SFLOAT;
-		attributeDesriptions[1].offset = offsetof(Vertex, color);
-		return attributeDesriptions;
+	static std::array<VkVertexInputAttributeDescription, 3> getAttributeDescriptions() {
+		std::array<VkVertexInputAttributeDescription, 3> attributeDescriptions = {};
+		attributeDescriptions[0].binding = 0;
+		attributeDescriptions[0].location = 0;
+		attributeDescriptions[0].format = VK_FORMAT_R32G32B32A32_SFLOAT;
+		attributeDescriptions[0].offset = offsetof(Vertex, pos);
+		attributeDescriptions[1].binding = 0;
+		attributeDescriptions[1].location = 1;
+		attributeDescriptions[1].format = VK_FORMAT_R32G32B32A32_SFLOAT;
+		attributeDescriptions[1].offset = offsetof(Vertex, color);
+		attributeDescriptions[2].binding = 0;
+		attributeDescriptions[2].location = 2;
+		attributeDescriptions[2].format = VK_FORMAT_R32G32_SFLOAT;
+		attributeDescriptions[2].offset = offsetof(Vertex, texCoord);
+		return attributeDescriptions;
 	}
 };
 
@@ -72,10 +77,10 @@ const std::vector<const char*> validationLayers = { "VK_LAYER_LUNARG_standard_va
 const std::vector<const char*> deviceExtensions = { VK_KHR_SWAPCHAIN_EXTENSION_NAME };
 //Array of vertex data for vertex shader
 const std::vector<Vertex> vertices = {
-	{ { -0.5f, -0.5f },{ 1.0f, 0.0f, 0.0f } },
-	{ { 0.5f, -0.5f },{ 0.0f, 1.0f, 0.0f } },
-	{ { 0.5f, 0.5f },{ 0.0f, 0.0f, 1.0f } },
-	{ { -0.5f, 0.5f },{ 1.0f, 1.0f, 1.0f } }
+	{ { -0.5f, -0.5f },{ 1.0f, 0.0f, 0.0f },{ 0.0f, 0.0f } },
+	{ { 0.5f, -0.5f },{ 0.0f, 1.0f, 0.0f },{ 1.0f, 0.0f } },
+	{ { 0.5f, 0.5f },{ 0.0f, 0.0f, 1.0f },{ 1.0f, 1.0f } },
+	{ { -0.5f, 0.5f },{ 1.0f, 1.0f, 1.0f },{ 0.0f, 1.0f } }
 };
 //Array that represents the contents of the index buffer
 const std::vector<uint16_t> indices = {
